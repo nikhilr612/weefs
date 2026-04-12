7ba0163cd3b549eac955818b170d70cef637f220
Prakyath P Nayak <prakyathpnayak@gmail.com>
2026-04-12 22:11:49 +0530

    Edit the entry point  to accomodate UI launch

--- src/io/wfs/main/App.java
+++ src/io/wfs/main/App.java
@@ -1,13 +1,16 @@
 package io.wfs.main;
 
+import io.wfs.ui.MainLauncher;
+
 public class App {
 
     public static void main(String[] args) throws Exception {
         if (args.length > 0 && "integration".equalsIgnoreCase(args[0])) {
             ArchiveIntegrationTest.run();
+        } else if (args.length > 0 && "gui".equalsIgnoreCase(args[0])) {
+            MainLauncher.main(args);
         } else {
             System.out.println("Usage: pass 'integration' to run archive round-trip checks.");
         }
     }
 }
-
